2f7950c6bc48dfddc3044b554b9336720d2eb4e8
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2026-02-01 20:59:22 +0900

    feat(nvim): bufferlineの閉じるボタンを非表示

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -13,6 +13,7 @@ return {
       },
       sections = {
         { section = "header" },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
         { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
         { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
         { section = "startup" },
