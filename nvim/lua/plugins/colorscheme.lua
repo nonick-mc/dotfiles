5f203e833a206d6a3fb7bd988e0f4427c0bc6593
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2026-04-26 03:33:42 +0900

    feat(nvim): colorschemeの設定を変更

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,33 @@
 return {
-  { "rebelot/kanagawa.nvim" },
-  { "xiyaowong/transparent.nvim" },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = function()
+      return {
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+
+            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          }
+        end,
+        transparent = true,
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
